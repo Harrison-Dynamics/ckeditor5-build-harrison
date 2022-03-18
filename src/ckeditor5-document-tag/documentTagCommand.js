335800f3941cf8802e4d0357e74062b870a04b50
Vlad Rotea <vlad.rotea@harrison.ro>
2022-03-18 09:12:30 +0200

    Document Tag CKEditor plugin

---
+++ src/ckeditor5-document-tag/documentTagCommand.js
@@ -0,0 +1,26 @@
+import Command from '@ckeditor/ckeditor5-core/src/command'
+
+export default class DocumentTagCommand extends Command {
+    execute({ tag }) {
+        const editor = this.editor
+        const selection = editor.model.document.selection
+
+        editor.model.change(writer => {
+            const documentTag = writer.createElement('documentTag', {
+                ...Object.fromEntries(selection.getAttributes()),
+                tag: tag
+            })
+
+            editor.model.insertContent(documentTag)
+        })
+    }
+
+    refresh() {
+        const model = this.editor.model
+        const selection = model.document.selection
+
+        const isAllowed = model.schema.checkChild(selection.focus.parent, 'documentTag')
+
+        this.isEnabled = isAllowed
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
 
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment'
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import Indent from '@ckeditor/ckeditor5-indent/src/indent'
+import Image from '@ckeditor/ckeditor5-image/src/image'
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption'
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle'
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar'
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload'
+import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter"
+import Link from '@ckeditor/ckeditor5-link/src/link'
+import List from '@ckeditor/ckeditor5-list/src/list'
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice'
+import Table from '@ckeditor/ckeditor5-table/src/table'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation'
+
+import DocumentTag from './ckeditor5-document-tag/documentTag'
+
 
 export default class HarrisonEditor extends ClassicEditorBase {}
 
@@ -26,7 +31,6 @@ HarrisonEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
-	BlockQuote,
 	Heading,
 	Image,
 	ImageCaption,
@@ -39,8 +43,9 @@ HarrisonEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
-];
+	TextTransformation,
+    DocumentTag,
+]
 
 HarrisonEditor.defaultConfig = {
     toolbar: [
@@ -48,7 +53,7 @@ HarrisonEditor.defaultConfig = {
       'link', '|',
       'numberedList', 'bulletedList', '|',
       'insertTable', 'tableColumn', 'tableRow', 'mergeTableCells', '|',
-      'undo', 'redo'
+      'undo', 'redo', 'documentTag'
     ],
   	image: {
 		toolbar: [
@@ -58,5 +63,5 @@ HarrisonEditor.defaultConfig = {
 		]
 	},
     language: 'ro'
-};
+}
 
